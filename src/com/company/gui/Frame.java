9012e22328d771a3a1947b089329ac8ec8ea5277
Sergei Galanin <sbgalanin@gmail.com>
2020-05-27 21:41:59 +0700

    initial commit

---
+++ src/com/company/gui/Frame.java
@@ -0,0 +1,15 @@
+package com.company.gui;
+
+import javax.swing.*;
+
+public class Frame extends JFrame {
+
+    public Frame() {
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+	    new Frame();
+    }
+}
